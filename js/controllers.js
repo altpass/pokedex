2daa8f0808fe51e7d01f7cf8a0e7cb118a7da65d
Artem <altpas@gmail.com>
2016-04-07 19:34:43 +0300

    checkbox fix

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,4 @@
-
-
 var pokemonListApp = angular.module('pokemonListApp', []);
-
-
 
 pokemonListApp
 	.controller('pokemonListCtrl', function ($scope, $http) {
@@ -17,39 +13,31 @@ pokemonListApp
 			$scope.filterVisible = "none";
 			
 			var addPokemonsToAll = function (data) {
-
 				var urls = data.results;
 
 				for(var i = 0; i < urls.length; i++){
 					var url = urls[i].url;
 					$http.get(url).success(function(data){												  
 						$scope.pokemons.push(data);
-
 					});
-				}								
-			}
+				}							
+			};
 
 			var getPokemons = function (limit, offset) {
 				$http.get('http://pokeapi.co/api/v2/pokemon/?limit=' + limit + '&offset=' + offset).success(function(data){  						
 					$scope.pokemonsForLoad = data;
 					addPokemonsToAll(data);
-
-
 					$scope.forLoadMore.push($scope.pokemonsForLoad);
 				});
 			};
 
-
-
 			getPokemons(limit, offset);	
-					
 
 			$scope.loadMore = function () {
 				offset += limit;				
 				getPokemons(limit, offset);				
 			};
 
-			
 			$scope.showDetail = function (pokemon) {
 				if ($scope.infoVisible == "none" || $scope.infoPokemon != pokemon) {
 					$scope.infoPokemon = pokemon;																				
@@ -57,8 +45,8 @@ pokemonListApp
 					$scope.infoTotalMoves = pokemon.moves.length;					
 					$scope.infoVisible = "block";					
 				} else {
-					$scope.infoVisible = "none"
-				};				
+					$scope.infoVisible = "none";
+				}
 			};
 
 			$scope.closeInfo = function() {
@@ -69,11 +57,9 @@ pokemonListApp
 				if ($scope.filterVisible == "none") {
 					$scope.filterVisible = "block";
 				} else {
-					$scope.filterVisible = "none"
+					$scope.filterVisible = "none";
 				}
-			}
-
-			
+			};
 
 	//POKEMONS end
 
@@ -90,7 +76,7 @@ pokemonListApp
 				    color += letters[Math.floor(Math.random() * 16)];
 				}
 				return color;
-			}
+			};
 
 			$http.get('http://pokeapi.co/api/v2/type/').success(function(data){  						
 					for(i = 0; i < data.results.length; i++){
@@ -101,19 +87,9 @@ pokemonListApp
 					for (i = 0; i < $scope.types.length; i++){
 						var name = $scope.types[i];
 						$scope.typesColor[name] = getRandomColor();
-
 					}
-
-
 				});
 			
-
-
-
-			
-
-
-
 	//TYPES end
 
 	//FILTER
@@ -124,10 +100,9 @@ pokemonListApp
 				} else {
 					$scope.allSelectedType.splice($scope.allSelectedType.indexOf(this.type), 1);
 				}				
-			}
+			};
 
 			$scope.filterByType = function (pokemon) {
-
 				for(var i = 0; i < pokemon.types.length; i++){
 					for(x = 0; x < $scope.allSelectedType.length; x++){
 						if(pokemon.types[i].type.name == $scope.allSelectedType[x]) {														
@@ -137,32 +112,33 @@ pokemonListApp
 				}				
 				return false;
 
-			}
+			};
 
-			$scope.sellectAllChange = "Unsellect All"
+			$scope.sellectAllChange = "Unsellect All";
+					$scope.selectedAll = true;				
 
 			$scope.sellectAll = function  (sellectAllTypes) {				
-
 				if(this.selectedAll) {
 					$scope.allSelectedType = $scope.types.slice();
-					$scope.sellectAllChange = "Unsellect All";				
+					$scope.sellectAllChange = "Unsellect All";
+					// $scope.selectedAll = false;
 				} else {
 					$scope.allSelectedType = [];
 					$scope.sellectAllChange = "Sellect All";										
 				}
-			}
+			};
 
 	//FILTER end
-})
+});
 
 
 pokemonListApp
 	.filter('capitalizeFirstLetter', function() {
 	 return function(input) {
-		if (input!=null){
+		if (input != null){
 			input = input.toLowerCase();
 			return input.substring(0,1).toUpperCase()+input.substring(1);
 		}
-	  }
+	  };
 	});
 
